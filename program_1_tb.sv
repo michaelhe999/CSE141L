e4431693713db9d7c8a2ae6718b3b7add9b30cb4
Michael He <michaelhe2016@gmail.com>
2025-06-12 16:47:02 -0700

    debug prints

--- program_1_tb.sv
+++ program_1_tb.sv
@@ -144,8 +144,8 @@ task automatic disp2(input logic [15:0] int_in);
 	// ✅ DEBUG PRINTS HERE
 	$display("[DEBUG] PC: %0d", f1.pc.current_pc_out);
 	$display("[DEBUG] instruction: %b", f1.im.instruction); 
-	$display("[DEBUG] done: %b", f1.control_decoder.done);
-	
+	$display("[DEBUG] done: %b", f1.cd.done);
+
   	flt_out  = {f1.dm.mem_core[3],f1.dm.mem_core[2]};	 // results from your memory
     flt_out0 = {f0.data_mem1.mem_core[3],f0.data_mem1.mem_core[2]};	 // results from my dummy DUT
     $display("what's feeding the case %b",int_in);

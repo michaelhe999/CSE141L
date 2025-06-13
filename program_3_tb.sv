14c62d4bcd868192ddf05cb669644f124d8b3351
Michael He <michaelhe2016@gmail.com>
2025-06-13 16:20:22 -0700

    debug statements back

--- program_3_tb.sv
+++ program_3_tb.sv
@@ -62,6 +62,21 @@ module program_3_tb();
 	  cycle_ct++;
   end
 
+  always @(posedge clk) begin
+		// Print every instruction :)
+        if (!f3.ack) begin
+		$display("[Cycle %0d] PC=%0d, inst=%b, done=%b, processor_running?=%b, ack=%b", 
+				t1.cycle_count, t1.pc.current_pc_out, t1.cd.instruction, t1.cd.done, t1.should_run_processor, t1.ack);
+        end
+        // if (f1.pc.current_pc_out%20 == 0) begin
+		// 	$display("[Cycle %0t] PC=%0d, inst=%b, done=%b", 
+		// 		$time, f1.pc.current_pc_out, f1.im.instruction, f1.cd.done);
+		// end
+		// if (cd.instruction == 9'b010000000) begin //done
+		// 	$display("[Cycle %0t] Done instruction PC=%0d, done=%b", $time, f1.pc.current_pc_out, f1.cd.done);
+		// end
+  end
+
   initial begin			          // contrived operands
     //#20ns reset = '0;
     flt1        = {1'b0, 5'h06, 10'b10_0000_0100};

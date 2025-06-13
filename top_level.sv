73137556f8bbb04f27778cb8892956ee2b69f6cc
Michael He <michaelhe2016@gmail.com>
2025-06-12 21:31:39 -0700

    cycle count lowered for debugging

--- top_level.sv
+++ top_level.sv
@@ -208,7 +208,7 @@ module top_level (
         cycle_count <= 0;
         overflow <= 0;
     end
-    else if(cycle_count >= 200)
+    else if(cycle_count >= 50)
         overflow = 1; 
     else if(ack == 0)   
         cycle_count <= cycle_count + 'b1;

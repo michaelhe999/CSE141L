c78ada06ef3fe451dcec7fe8e5aa5c52640a1f86
Michael He <michaelhe2016@gmail.com>
2025-06-13 21:51:04 -0700

    exp shifting fix

--- alu.sv
+++ alu.sv
@@ -5,6 +5,8 @@ module alu (
     output logic [7:0] alu_out, // result of the operation
     output logic zero // 1 if result is 0, 0 otherwise 
 );
+
+logic [7:0] test_overflow ; // temporary variable for overflow output
     always_comb begin
         case (alu_opcode)
             3'b000: alu_out = alu_input_a & alu_input_b; // bitwise AND
@@ -14,7 +16,11 @@ module alu (
             3'b100: alu_out = alu_input_a << alu_input_b; // shift left logical
             3'b101: alu_out = alu_input_a >> alu_input_b; // shift right logical
             3'b110: alu_out = (alu_input_a != alu_input_b) ? 8'b00000001 : 8'b0; // compare not equal
-            3'b111: alu_out = (alu_input_a + alu_input_b > 9'b011111111) ? 8'b00000001 : 8'b0; // overflow check
+            3'b111: begin
+                test_overflow = R1 + R2; // temporary variable for overflow
+                alu_out = ~(alu_input_a[7] & alu_input_b[7] == test_oveflow[7]);
+            end
+
         endcase
 
         zero = (alu_out == 8'b0); // set zero flag if result is zero

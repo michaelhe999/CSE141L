2214a840e0bf29d2570562eb66f62a3995dad41d
Michael He <michaelhe2016@gmail.com>
2025-06-13 21:29:57 -0700

    mantissa fix machine code

--- instruction_memory.sv
+++ instruction_memory.sv
@@ -10,7 +10,7 @@ module instruction_memory(
     initial begin
         //$readmemb("C:/Users/mih024/Desktop/CSE_141L_Project_Files/CSE141L/test2_program1.txt", memory_array); // load instruction memory from file
         //$readmemb("C:/Users/mih024/Desktop/CSE_141L_Project_Files/CSE141L/test2_program2.txt", memory_array);
-        $readmemb("C:/Users/mih024/Desktop/CSE_141L_Project_Files/CSE141L/test4_program3.txt", memory_array);
+        $readmemb("C:/Users/mih024/Desktop/CSE_141L_Project_Files/CSE141L/test5_program3.txt", memory_array);
     end
 
     always_comb begin
